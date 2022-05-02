bc5b9a945e138f7b6415cc645e46b6535c24ec68
Shakil Ahmed <73547483+sha2992@users.noreply.github.com>
2022-05-02 09:36:43 +0600

    Update Credit Data Cleaning.sql

--- Data Cleaning/Credit Data Cleaning.sql
+++ Data Cleaning/Credit Data Cleaning.sql
@@ -1,44 +1,44 @@
 select * from [Credit Card Database]..demographics;
 
---alter table [Credit Card Database]..demographics add UpdatedGender varchar(10)
---alter table [Credit Card Database]..demographics add Marital_Status varchar(10)
---alter table [Credit Card Database]..demographics add Employment varchar(15)
+alter table [Credit Card Database]..demographics add UpdatedGender varchar(10);
+alter table [Credit Card Database]..demographics add Marital_Status varchar(10);
+alter table [Credit Card Database]..demographics add Employment varchar(15);
 
 -- converting Gender 1 to Male and 0 to Female
---update [Credit Card Database]..demographics 
---set UpdatedGender='Male'
---where Gender=1
+update [Credit Card Database]..demographics 
+set UpdatedGender='Male'
+where Gender=1;
 
---update [Credit Card Database]..demographics 
---set UpdatedGender='Female'
---where Gender=0
+update [Credit Card Database]..demographics 
+set UpdatedGender='Female'
+where Gender=0;
 
---Drop table [Credit Card Database]..demographics 
+Drop table [Credit Card Database]..demographics ;
 
 -- converting Marital Status 
---update [Credit Card Database]..demographics
---set Marital_Status = 'Married'
---where Married = 1
+update [Credit Card Database]..demographics
+set Marital_Status = 'Married'
+where Married = 1;
 
---update [Credit Card Database]..demographics
---set Marital_Status = 'Unmarried'
---where Married = 0
+update [Credit Card Database]..demographics
+set Marital_Status = 'Unmarried'
+where Married = 0;
 
 -- converting Employment
---update [Credit Card Database]..demographics
---set Employment = 'Employed' 
---where Employed = 1
+update [Credit Card Database]..demographics
+set Employment = 'Employed' 
+where Employed = 1;
 
---update [Credit Card Database]..demographics
---Set Employment = 'Unemployed'
---where Employed = 0
+update [Credit Card Database]..demographics
+Set Employment = 'Unemployed'
+where Employed = 0;
 
-alter table [Credit Card Database]..demographics drop column Gender
-alter table [Credit Card Database]..demographics drop column  Married
-alter table [Credit Card Database]..demographics drop column Employed
+alter table [Credit Card Database]..demographics drop column Gender;
+alter table [Credit Card Database]..demographics drop column  Married;
+alter table [Credit Card Database]..demographics drop column Employed;
 
-select MAX(age), MIN(age) from [Credit Card Database]..demographics
+select MAX(age), MIN(age) from [Credit Card Database]..demographics;
 
-select ROUND(Age,0) from [Credit Card Database]..Demographics
+select ROUND(Age,0) from [Credit Card Database]..Demographics;
 
-select FLOOR(Age) from [Credit Card Database]..Demographics
+select FLOOR(Age) from [Credit Card Database]..Demographics;
